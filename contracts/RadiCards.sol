7d5178a57b9f198b72bd1143906c434e66edd219
Andy Gray <andy@whatsthescore.co.uk>
2018-12-05 22:12:21 +0000

    tidy up

--- contracts/RadiCards.sol
+++ contracts/RadiCards.sol
@@ -16,9 +16,7 @@ contract RadiCards is ERC721Token, Whitelist {
 
   string public tokenBaseURI = "https://ipfs.infura.io/ipfs/";
 
-  // A pointer to the next token to be minted, zero indexed
   uint256 public tokenIdPointer = 0;
-
   uint256 public minContribution = 0.05 ether;
 
   struct Benefactor {
@@ -78,7 +76,7 @@ contract RadiCards is ERC721Token, Whitelist {
     require(benefactors[_benefactor].ethAddress != address(0), "Must specify existing benefactor");
 
     _mint(to, tokenURI);
-
+    
     benefactors[_benefactor].ethAddress.transfer(msg.value);
 
     return true;
@@ -96,15 +94,6 @@ contract RadiCards is ERC721Token, Whitelist {
   function burn(uint256 tokenId) public onlyIfWhitelisted(msg.sender) {
     // Super burn
     _burn(ownerOf(tokenId), tokenId);
-  }
-
-  function setTokenBaseURI(string _newBaseURI) external onlyIfWhitelisted(msg.sender) {
-    require(bytes(_newBaseURI).length != 0, "Base URI invalid");
-    tokenBaseURI = _newBaseURI;
-  }
-
-  function setMinContribution(uint256 _minContribution) external onlyIfWhitelisted(msg.sender) {
-    minContribution = _minContribution;
   }
 
   function tokenURI(uint256 _tokenId) public view returns (string) {
@@ -140,5 +129,14 @@ contract RadiCards is ERC721Token, Whitelist {
       _description
     );
     cardsIndex.push(_index);
+  }
+
+  function setTokenBaseURI(string _newBaseURI) external onlyIfWhitelisted(msg.sender) {
+    require(bytes(_newBaseURI).length != 0, "Base URI invalid");
+    tokenBaseURI = _newBaseURI;
+  }
+
+  function setMinContribution(uint256 _minContribution) external onlyIfWhitelisted(msg.sender) {
+    minContribution = _minContribution;
   }
 }
