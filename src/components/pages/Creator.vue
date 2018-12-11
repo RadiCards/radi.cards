8f8c4bcc4a494b027137dddf33d1283a9d04958e
Christopher Maree <christopher.maree@gmail.com>
2018-12-11 16:45:33 +0200

    added custom accordian

--- src/components/pages/Creator.vue
+++ src/components/pages/Creator.vue
@@ -10,7 +10,7 @@
       </div>
     </div>
     <form>
-      <form-wizard
+      <!-- <form-wizard
         title
         subtitle
         color="#333333"
@@ -117,11 +117,62 @@
               <code>{{formData.recipient}}</code>
             </p>
             <hr>
-            <!-- <pre class="small">{{generateIpfsData()}}</pre> -->
           </div>
-          <!-- <button type="button" class="btn btn-primary" v-on:click="giveBirth">Create</button> -->
         </tab-content>
-      </form-wizard>
+      </form-wizard>-->
+      <div role="tablist">
+        <b-card no-body class="mb-1">
+          <b-card-header header-tag="header" class="p-1" role="tab">
+            <b-btn block href="#" v-b-toggle.accordion1 variant="info">Accordion 1</b-btn>
+          </b-card-header>
+          <b-collapse id="accordion1" visible accordion="my-accordion" role="tabpanel">
+            <b-card-body>
+              <section class="section">
+                <h4 class="section__title">STEP ONE</h4>
+                <h2>Choose your favourite radicard</h2>
+                <p>All unique designs</p>
+
+                <div class="card-slider" v-if="cards && cards.length > 0">
+                  <card v-for="item in cards" :key="item.tokenId" :cdata="item">{{item}}</card>
+                </div>
+              </section>
+            </b-card-body>
+          </b-collapse>
+        </b-card>
+        <b-card no-body class="mb-1">
+          <b-card-header header-tag="header" class="p-1" role="tab">
+            <b-btn block href="#" v-b-toggle.accordion2 variant="info">Accordion 2</b-btn>
+          </b-card-header>
+          <b-collapse id="accordion2" accordion="my-accordion" role="tabpanel">
+            <b-card-body>
+              <section class="section">
+                <h4 class="section__title">STEP TWO</h4>
+                <h2>Choose a project to support</h2>
+                <p>From environmental protection to online privacy rights</p>
+
+                <div class="charities" v-if="benefactors && benefactors.length > 0">
+                  <benefactor v-for="item in benefactors" :key="item.address" :benefactor="item"></benefactor>
+                </div>
+              </section>
+            </b-card-body>
+          </b-collapse>
+        </b-card>
+        <b-card no-body class="mb-1">
+          <b-card-header header-tag="header" class="p-1" role="tab">
+            <b-btn block href="#" v-b-toggle.accordion3 variant="info">Accordion 3</b-btn>
+          </b-card-header>
+          <b-collapse id="accordion3" accordion="my-accordion" role="tabpanel">
+            <b-card-body>
+              <section class="section">
+                <h4 class="section__title">STEP THREE</h4>
+                <h2>Send radicards to friends and family</h2>
+                <p>Spread hope & joy</p>
+                <samplequote></samplequote>
+              </section>
+            </b-card-body>
+          </b-collapse>
+        </b-card>
+      </div>
     </form>
   </div>
 </template>
@@ -146,6 +197,16 @@ export default {
         errors: [],
         card: {}
       },
+      text: `
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
+        brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon
+        tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
+        assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
+        wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher
+        vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic
+        synth nesciunt you probably haven't heard of them accusamus labore VHS.
+      `,
       response: {
         ipfsHash: null
       }
@@ -207,7 +268,7 @@ export default {
 @import "../../styles/variables.scss";
 @import "../../styles/variables.scss";
 .card-selected {
-  margin-top:-50px;
+  margin-top: -50px;
   transition: all 0.2s ease-in-out;
 }
 </style>
