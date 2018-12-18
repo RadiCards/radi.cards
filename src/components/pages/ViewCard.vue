fd441da7922e779366db18a1c5817a96421bdeb2
Christopher Maree <christopher.maree@gmail.com>
2018-12-18 13:08:39 +0200

    added text on deep url

--- src/components/pages/ViewCard.vue
+++ src/components/pages/ViewCard.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="container">
-    <card :cdata="deepUrlCard"/>
+    <div class="row">
+      <div class="col">
+        <card :cdata="deepUrlCard"/>
+      </div>
+      <div class="col text-center">
+        <img src="/static/icons/gift.png" class="pt-5" alt style="width: 4rem;">
+        <p class="pt-4">RadiCards lets you send e-cards to your friends and family using crypto! 100% of the purchase goes to charities fighting for freedom around the world.</p>
+      <hr class="pb-4">
+      <router-link :to="{ name: 'cardshop' }" class="transferButton mt-2">Gift another card like this</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -32,9 +42,18 @@ export default {
 
 <style lang="scss" scoped>
 @import "../../styles/variables.scss";
-@import "../../styles/variables.scss";
 .card-selected {
   margin-top: -25px;
   transition: all 0.2s ease-in-out;
 }
+  .transferButton {
+    background: #000000;
+    font-family: Helvetica;
+    line-height: normal;
+    text-align: center;
+    color: #ffffff;
+    font-size: 16px;
+    padding: 15px;
+    border: 1px solid black;
+  }
 </style>
