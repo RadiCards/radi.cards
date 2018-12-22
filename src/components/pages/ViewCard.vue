5a7378c0ef7d50a52f4f7aaa24669a685eb27b2e
Lili <li.saengsanthitam@gmail.com>
2018-12-22 12:50:11 +0100

    Add flip to see message

--- src/components/pages/ViewCard.vue
+++ src/components/pages/ViewCard.vue
@@ -3,6 +3,8 @@
     <b-row>
       <b-col cols="12" md="6" style="display: flex; justify-content: center; padding-left: 2rem;">
         <card :cdata="deepUrlCard"/>
+        <img src="/static/images/red-arrow.svg" class="dl-1">
+        <span class="cheeky-comment">click on the card to see the hidden message.</span>
       </b-col>
       <b-col cols="12" md="6" class="text-center">
         <img src="/static/icons/gift.png" class="pt-5" alt style="width: 4rem;">
@@ -41,6 +43,8 @@ export default {
 
 <style lang="scss" scoped>
 @import "../../styles/variables.scss";
+
+
 .card-selected {
   margin-top: -25px;
   transition: all 0.2s ease-in-out;
@@ -55,4 +59,25 @@ export default {
     padding: 15px;
     border: 1px solid black;
   }
+
+.cheeky-comment {
+  position: absolute;
+  top: 60%;
+  left: 82%;
+  width: 9rem;
+  transform: rotate(6deg);
+  font-weight: bold;
+}
+.dl-1{
+  -moz-transform: scaleX(-1);
+  -o-transform: scaleX(-1);
+  -webkit-transform: scaleX(-1);
+  transform: scaleX(-1);
+  filter: FlipH;
+  -ms-filter: "FlipH";
+  width: 12%;
+
+}
+
+
 </style>
