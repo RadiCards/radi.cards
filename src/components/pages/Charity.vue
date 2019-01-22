b81b8f5345373222a6fe5e508b22f75eb21efb99
Lili <li.saengsanthitam@gmail.com>
2019-01-22 16:19:45 +0100

    update charity page

--- src/components/pages/Charity.vue
+++ src/components/pages/Charity.vue
@@ -1,9 +1,118 @@
 <template>
   <section class="section section--charity">
-
     <h2>Charity</h2>
+
+    <h3>
+      <a href="https://www.grassrootseconomics.org/">Grass Roots Economics</a>
+      <p class="p--small">
+        <a href="https://etherscan.io/address/0x72f8547fae0F65C55f3BD0dD991c9A132C447ce6">etherscan</a>
+      </p>
+    </h3>
+
+    <p>
+      Grassroots Economics Foundation, based in Kenya,
+      empowers marginalized communities in Africa to trade their own community specific currencies,
+      using blockchain technology, to pay for food, education, and health services.
+      Local blockchain-based currencies give these communities the tools to pull themselves out of poverty
+      by tapping into their underutilized capacity, helping them to develop stable and resilient local economies and
+      connecting them to regional markets.
+      We operate under the fundamental assumption that the goods, resources,
+      and human capacity exist in these communities - the only thing that’s missing is money.
+    </p>
+
+    <p>
+      Grassroots enables each community to create and manage their own digital community currency on the blockchain,
+      ensuring fast, auditable trading with no transaction fees.
+      Grassroots engages a team of local leaders to oversee all onboarding and training of businesses and individuals.
+      Each of these community currencies is exchangeable with other community currencies in the region via an open source protocol,
+      creating thriving regional economies.
+      The system works on feature phones via USSD and doesn’t require smart phones or internet,
+      allowing anyone to utilize blockchain technology.
+    </p>
+
+    <p class="p--small">
+      <a href="https://www.youtube.com/watch?v=UqobcADSUTQ">Learn more</a>
+    </p>
+    <br>
+    <br>
+
+    <h3>
+      <a href="https://www.bitcoinvenezuela.com/">Bitcoin Venezuela</a>
+      <p class="p--small">
+        <a href="https://etherscan.io/address/0x5f48465bb9a29a3904a8d320146e78640df0e96e">etherscan</a>
+      </p>
+    </h3>
+
+    <p>
+      We at <a href="http://BitcoinVenezuela.com">BitcoinVenezuela</a>'s been educating about Bitcoin in #Venezuela and Spain since 2012.
+
+      We've been doing humanitarian aid for 3+ years through #Bitcoin & cryptos donations.
+      W/ private donators we’re helping reconstruct a school-farm,
+      we've made water wells, we're buying cookware for another soup kitchen.
+      We provide a soup kitchen the weekly non-cooked food that is served to 1600+ people daily. 
+    </p>
+    <p>
+      We've been running the website as nonprofit with free Live Chat support for years,
+      curated content for first steps in #Bitcoin.
+      The admin of BTC Venezuela worked at @Coinbase & now @metamask_io.
+      We give consulting to projects wanting to launch in #Venezuela.
+      We help @GiveCrypto activities/tests in 🇻🇪
+    </p>
+
+  
+
+    <p class="p--small">
+      <a href="https://www.youtube.com/watch?v=UqobcADSUTQ">Learn more</a>
+    </p>
+    <br>
+    <br>
+
+    <h3>
+      <a href="https://www.grassrootseconomics.org/">Grass Roots Economics</a>
+      <p class="p--small">
+        <a href="https://etherscan.io/address/0x72f8547fae0F65C55f3BD0dD991c9A132C447ce6">etherscan</a>
+      </p>
+    </h3>
+
+    <p>
+      Grassroots Economics Foundation, based in Kenya,
+      empowers marginalized communities in Africa to trade their own community specific currencies,
+      using blockchain technology, to pay for food, education, and health services.
+      Local blockchain-based currencies give these communities the tools to pull themselves out of poverty
+      by tapping into their underutilized capacity, helping them to develop stable and resilient local economies and
+      connecting them to regional markets.
+      We operate under the fundamental assumption that the goods, resources,
+      and human capacity exist in these communities - the only thing that’s missing is money.
+    </p>
+
+    <p>
+      Grassroots enables each community to create and manage their own digital community currency on the blockchain,
+      ensuring fast, auditable trading with no transaction fees.
+      Grassroots engages a team of local leaders to oversee all onboarding and training of businesses and individuals.
+      Each of these community currencies is exchangeable with other community currencies in the region via an open source protocol,
+      creating thriving regional economies.
+      The system works on feature phones via USSD and doesn’t require smart phones or internet,
+      allowing anyone to utilize blockchain technology.
+    </p>
+
+    <p class="p--small">
+      <a href="https://www.youtube.com/watch?v=UqobcADSUTQ">Learn more</a>
+    </p>
+    <br>
+    <br>
+
+    <p>
+      If you’d like to help buidl, want donate some art/memes/cat pictures or get your non-profit onto the RadiCards platform, drop us a line at
+      <span>
+        <a href="mailto:radicals@radi.cards">radicals@radi.cards</a>
+      </span>or in
+      <span>
+        <a href="https://t.me/joinchat/GBiop1dC2yYsgFwo4O3gMA">our Telegram group!</a>
+      </span>
+    </p>
   </section>
 </template>
+
 
 <script>
 import { mapGetters, mapState } from "vuex";
@@ -18,12 +127,18 @@ export default {
 
 <style lang="scss" scoped>
 .section--charity {
-  h1, h2 {
+  h1,
+  h2 {
     margin-bottom: 2rem;
   }
-  p, h3, h4, ul, ol {
+  p,
+  h3,
+  h4,
+  ul,
+  ol {
     margin-bottom: 1rem;
   }
+
   ul {
     list-style: square;
     padding-left: 1rem;
