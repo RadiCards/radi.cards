979e772ab1bb36b2f263f6ea25d996cf8b5650a7
James Morgan <james.morgan.e@googlemail.com>
2019-01-28 09:58:56 +0000

    new cards

---
+++ migrations/5_add_radi-cards-batch-2-unlimted.js
@@ -0,0 +1,42 @@
+const RadiCards = artifacts.require("./RadiCards.sol");
+
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const infuraApikey = "4ed01157025d44b0b0ad5932e1d877ea";
+
+module.exports = async (deployer, network, accounts) => {
+  let account = accounts[0];
+
+  // Load in other accounts for different networks
+  if (
+    network === "ropsten" ||
+    network === "ropsten-fork" ||
+    network === "rinkeby" ||
+    network === "rinkeby-fork" ||
+    network === "kovan" ||
+    network === "kovan-fork"
+  ) {
+    account = new HDWalletProvider(
+      require("../mnemonic"),
+      `https://${network}.infura.io/v3/${infuraApikey}`,
+      0
+    ).getAddress();
+  }
+
+  if (network === "live" || network === "live-fork") {
+    account = new HDWalletProvider(
+      require("../mnemonic_live"),
+      `https://mainnet.infura.io/v3/${infuraApikey}`,
+      0
+    ).getAddress();
+  }
+
+  console.log(`Running within network = ${network}`);
+  console.log(`Account = ${account}`);
+
+  const contract = await RadiCards.deployed();
+  await contract.addCard(3, "QmZoBtnkDXXiod8CFyrXyun6CTWi826jwHhMhPPqc2Bjp2", true, 0, 0, {from: account}); // Ophelia Fu	- Ink Fireworks
+  await contract.addCard(4, "QmcNqjBXr1hwL6xHhigutMKcqACqa9aNysDGWzgN6N3QVr", true, 0, 0, {from: account}); // Pet3rpan - Chinese NoYuan
+  await contract.addCard(5, "QmWfXVp37surPRAwSX9bf5MJ2WfiN94mqgcGwJaJoM54m3", true, 0, 0, {from: account}); // Joyful IN PIG YEAR
+  await contract.addCard(6, "Qmdwb211MBb2n5BkYF6p6xr7QkPte669kQCiJveQiAtTkb", true, 0, 0, {from: account}); //Aktiv Protesk	- Pinki.B
+  await contract.addCard(7, "QmSe6BLCR17kvBh94HuHreWPry5VEzorEpeuGHiCEwCg6M", true, 0, 0, {from: account}); //Aktiv Protesk	- Pinki.B
+};
